2d5c8c6b93daf3cad3e3b1e23060a1dc9bdca367
will-shaw <will_shaw@live.com>
2017-10-09 15:53:50 +1300

    Added input reading for scenarios. Added sample input.

--- ArbIntMain.java
+++ ArbIntMain.java
@@ -1,8 +1,70 @@
 import java.math.BigInteger;
+import java.util.Scanner;
 
 public class ArbIntMain {
 
-    public static void main(String[]args) {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String line;
+
+        while (sc.hasNextLine()) {
+            line = sc.nextLine();
+
+            if (!line.isEmpty() && !line.matches("^#.*")) {
+
+                String[] tokens = line.split(" ");
+
+                if (tokens.length > 3) {
+                    System.out.println(line);
+                    System.out.println("# Syntax error");
+                    System.exit(1);
+                }
+
+                ArbInt firstToken = new ArbInt(tokens[0]);
+
+                String secondToken = tokens[1];
+
+                ArbInt thirdToken = new ArbInt(tokens[2]);
+
+                System.out.println(line);
+                switch (secondToken) {
+                case "+":
+                    System.out.println("# " + firstToken.add(thirdToken));
+                    break;
+                case "-":
+                    System.out.println("# " + firstToken.subtract(thirdToken));
+                    break;
+                case "*":
+                    System.out.println("Not yet supported");
+                    break;
+                case "/":
+                    System.out.println("Not yet supported");            
+                    break;
+                case "=":
+                    System.out.println("# " + firstToken.compare(thirdToken, 0));
+                    break;
+                case "<":
+                    System.out.println("# " + firstToken.compare(thirdToken, 1));
+                    break;
+                case ">":
+                    System.out.println("# " + firstToken.compare(thirdToken, 2));
+                    break;
+                case "gcd":
+                    System.out.println("Not yet supported");            
+                    break;
+                default:
+                    System.out.println("Syntax error");
+                    break;
+                }
+                System.out.println();
+            }
+        }
+
+        sc.close();
+
+        /*
         ArbInt ai = new ArbInt("12345678901234567890123");
         ArbInt ai2 = new ArbInt("765432109877");
         ArbInt ai3 = new ArbInt("1234567890123");
@@ -20,5 +82,6 @@ public class ArbIntMain {
         System.out.println(ai.subtract(ai4));
         System.out.println(ai6.subtract(ai7));
         System.out.println(ai8.halve());
+        */
     }
 }
